310d245bf5d0c2ba8f9097c99cc235278f106acf
Yuki Ono <108729931+KTOPSOFT@users.noreply.github.com>
2024-04-02 03:57:39 +0900

    WebGL Genshin Royal Alpha success

---
+++ Assets/Genshin Royal/KTOP_Splash/Splash_Handler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash_Handler : MonoBehaviour
+{
+    public void LoadSuntail()
+    {
+        SceneManager.LoadScene("Suntail Village");
+    }
+}
